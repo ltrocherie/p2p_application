0e88eb1105081e25c839dad26c718e5b66cade08
Laurent Genty <laurent.genty@enseirb-matmeca.fr>
2020-03-30 16:51:32 +0200

    adding new piecesize field to struct file

--- tracker/src/hash_table.h
+++ tracker/src/hash_table.h
@@ -12,14 +12,19 @@ struct owner {
     SLIST_ENTRY(owner) next_owner;
 };
 
-//Structure de fichier
+/**
+ * \struct file hash_table.h
+ */
 struct file {
-        char *key;
-        SLIST_HEAD(,owner) owners;
-        int nb_owners;
-        char *name;
-        int length;
-        SLIST_ENTRY(file) next_file;
+        char *key; /** pseudo-unique key of the file */
+        char *name; /** filename */
+        int length; /** total length of the file */
+        int piecesize; /** size of the piece */
+
+        int nb_owners; /** total number of owners */
+        SLIST_HEAD(,owner) owners; /** list of owners of the file */
+
+        SLIST_ENTRY(file) next_file; /** next_file for queue bsd lib */
 };
 
 //Table de hachage
@@ -29,7 +34,7 @@ SLIST_HEAD(,file) hash_table[HASH_TABLE_LENGTH];
 void hash_table_init();
 
 //Ajoute ou met à jour un fichier de clé fey
-void add(char* key,char* IP, int port,char* name, int length);
+void add(char* key,char* IP, int port,char* name, int length, int piecesize);
 
 //Recherche un fichier à partir de sa clé key et renvoie le fichier f
 void search(char* key,struct file *f);
