9da6a10e91aefc3d94f86655c3ba450f37bb945a
DUCHEMIN Emeric <educhemin001@enseirb-matmeca.fr>
2020-04-28 02:29:36 +0200

    Refactored AnnounceToTracker to allow announce with interface handling

--- peer/src/AnnounceToTracker.java
+++ peer/src/AnnounceToTracker.java
@@ -1,38 +1,53 @@
 import java.io.*;
 import java.net.*;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.ListIterator;
+
 /**
  * */
 
-public class AnnounceToTracker extends PeerConfig{
+public class AnnounceToTracker extends PeerConfig implements Sender{
 
 
-    public void announceTracker(String[] args, String folderName) throws Exception{ // on devrait pas passer le dossier en paramètre ?
+    public void sendMessage(ArrayList<JTextField> texts){ // on devrait pas passer le dossier en paramètre ?
       /*
           TODO : Rules announce, look, getfile
       */
-        if(args.length>0){
-            super.port = Integer.parseInt(args[0]);
-        }
-        if(args.length>1){
-            super.trackerIp = args[1];
-        }
-        if(args.length>2){
-            super.trackerPort = Integer.parseInt(args[2]);
+        try{
+            Socket socket = new Socket(super.trackerIp,super.trackerPort);
+            /*File[] fileL = super.fileList("../seed"); // This fonctionne pas normalement en ce moment.
+            String message = super.parseFileList(fileL);*/
+            DatFileParser getMessage= new DatFileParser();
+            String message = getMessage.getFilesFrom(super.seedFile);
+            message = "announce listen " + super.inPort + " seed [" + message;
+            message = message.substring(0,message.length() - 1) + "]";
+            System.out.println(message);
+            BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            PrintWriter pw = new PrintWriter(new BufferedWriter(new OutputStreamWriter(socket.getOutputStream())),true);
+            pw.println(message);
+            System.out.println(">"+message);
+            String str = br.readLine();// Ca c'est pour suivre en temps réel sur le terminal.
+            System.out.println("<"+str);
+            pw.println("END");
+            pw.close();
+            br.close();
+            socket.close();
+        }catch(Exception e){
+            System.out.println("Socket connecting error");
         }
 
-        Socket socket = new Socket("localhost",super.trackerPort);
-        File[] fileL = super.fileList(folderName); // This fonctionne pas normalement en ce moment.
-        String message = super.parseFileList(fileL);
-        System.out.println(message);
-        BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-        PrintWriter pw = new PrintWriter(new BufferedWriter(new OutputStreamWriter(socket.getOutputStream())),true);
-        pw.println(message);
-        String str = br.readLine();// Ca c'est pour suivre en temps réel dans le log.
-        System.out.println(str);
-        pw.println("END");
-        pw.close();
-        br.close();
-        socket.close();
+    }
+    public void addValues(ArrayList<JTextField> texts){
+
+    }
+
+    public void flush(ArrayList<JTextField> texts){
 
     }
 
